2ab32d019c4c92a523b966e04e83fcea9a0eb02a
Paul Schaefer <yeppaul2@gmail.com>
2015-12-10 16:08:21 -0500

    Uploaded start page

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php
+	$bg = array('bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png', 'bg6.png', 'bg7.png', 'bg8.png', 'bg9.png', 'bg10.png', 'bg11.png', 'bg12.png', 'bg13.png', 'bg14.png', 'bg15.png', 'bg16.png', 'bg17.png', 'bg18.png', 'bg19.png', 'bg20.png', 'bg21.png', 'bg22.png');
+	$i = rand(0, count($bg)-1);
+  	$selectedBg = "$bg[$i]";
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>New Tab</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="style.css">
+		<style type="text/css">
+			body { background: url(../../img/trippy/<?php echo $selectedBg; ?>) center 100%; }
+		</style>
+	</head>
+	<body>
+		<canvas style="z-index: 9" id="snow"></canvas>
+		<div class="wrapper">
+			<ul>
+				<p class="center">Howdy! In <span class="location">Hell</span>, the weather is <span class="current">null</span>, the temperature is <span class="temp">--</span>&deg;, and the wind is <span class="wind">0mph N</span>.</p>
+				<div class="left">
+					<li><a id="parent1" class="parent" href="javascript:void(0)">Social Media<span class="right">s</span></a>
+						<ul class="subMenu">
+							<li><a class="tab1" href="https://facebook.com/">Facebook<span class="right">f</span></a></li>
+							<li><a class="tab1" href="https://messenger.com/">Messenger<span class="right">m</span></a></li>
+							<li><a class="tab1" href="https://instagram.com/">Instagram<span class="right">i</span></a></li>
+							<li><a class="tab1" href="https://tumblr.com/">Tumblr<span class="right">t</span></a></li>
+							<li><a class="tab1" href="https://twitter.com/">Twitter<span class="right">w</span></a></li>
+							<li><a class="tab1" href="https://mail.google.com/">Gmail<span class="right">g</span></a></li>
+						</ul>
+					</li>
+					<li><a id="parent2" class="parent" href="javascript:void(0)">Music &amp; Videos<span class="right">m</span></a>
+						<ul class="subMenu">
+							<li><a class="tab2" href="https://youtube.com/">YouTube<span class="right">y</span></a></li>
+							<li><a class="tab2" href="https://soundcloud.com/">SoundCloud<span class="right">c</span></a></li>
+							<li><a class="tab2" href="https://music.google.com/">Google Play Music<span class="right">g</span></a></li>
+							<li><a class="tab2" href="https://play.spotify.com/">Spotify Web Client<span class="right">s</span></a></li>
+						</ul>
+					</li>
+					<li><a id="parent3" class="parent" href="javascript:void(0)">Reddit<span class="right">r</span></a>
+						<ul class="subMenu">
+							<li><a class="tab3" href="https://reddit.com/">Front Page<span class="right">f</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/trees/">/r/trees<span class="right">t</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/see/">/r/see<span class="right">s</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/blackpeopletwitter/">/r/blackpeopletwitter<span class="right">b</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/showerthoughts/">/r/showerthoughts<span class="right">w</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/unixporn/">/r/unixporn<span class="right">u</span></a></li>
+							<li><a class="tab3" href="https://reddit.com/r/startpages/">/r/startpages<span class="right">x</span></a></li>
+						</ul>
+					</li>
+				</div>
+				<div class="right">
+					<li><a id="parent4" class="parent" href="javascript:void(0)">Research &amp; Dev<span class="right">?</span></a>
+						<ul class="subMenu">
+							<li><a class="tab4" href="https://stackoverflow.com/">Stack Overflow<span class="right">s</span></a></li>
+							<li><a class="tab4" href="https://developer.mozilla.com/">Mozilla Development Network<span class="right">m</span></a></li>
+							<li><a class="tab4" href="https://github.com/">Github<span class="right">g</span></a></li>
+							<li><a class="tab4" href="https://askubuntu.com/">Ask Ubuntu<span class="right">u</span></a></li>
+							<li><a class="tab4" href="https://www.google.com/fonts/">Google Web Fonts<span class="right">f</span></a></li>
+							<li><a class="tab4" href="https://unsplash.com/">Unsplash<span class="right">p</span></a></li>
+							<li><a class="tab4" href="http://en.wikipedia.org/">Wikipedia<span class="right">w</span></a></li>
+							<li><a class="tab4" href="http://wolframalpha.com/">WolframAlpha<span class="right">a</span></a></li>
+							<li><a class="tab4" href="https://niice.co/">Niice<span class="right">n</span></a></li>
+							<li><a class="tab4" href="https://www.google.com/images/">Google Images<span class="right">i</span></a></li>
+						</ul>
+					</li>
+					<li><a id="parent5" class="parent" href="javascript:void(0)">o2<span class="right">o</span></a>
+						<ul class="subMenu">
+							<li><a class="tab5" href="http://life.o2dca.com/">Life<span class="right">l</span></a></li>
+							<li><a class="tab5" href="http://admin.o2dca.com/">Admin<span class="right">a</span></a></li>
+							<li><a class="tab5" href="https://www.toggl.com/app/timer">Toggl<span class="right">t</span></a></li>
+							<li><a class="tab5" href="http://bugs.o2dca.com/">Mantis<span class="right">m</span></a></li>
+							<li><a class="tab5" href="https://mail.google.com/mail/u/1/#inbox">Gmail<span class="right">g</span></a></li>
+						</ul>
+					</li>
+					<li><a id="parent6" class="parent" href="javascript:void(0)">Comics<span class="right">c</span></a>
+						<ul class="subMenu">
+							<li><a class="tab6" href="http://xkcd.com/">XKCD<span class="right">x</span></a></li>
+							<li><a class="tab6" href="http://explosm.net/">Cyanide and Happiness<span class="right">&</span></a></li>
+							<li><a class="tab6" href="http://potshotcomic.com/">Pot Shot Comic<span class="right">p</span></a></li>
+							<li><a class="tab6" href="http://powernapcomic.com/">Powernap Comic<span class="right">n</span></a></li>
+							<li><a class="tab6" href="http://extrafabulouscomics.com/">Extra Fabulous Comics<span class="right">e</span></a></li>
+							<li><a class="tab6" href="http://thedoghousediaries.com/">Doghouse Diaries<span class="right">d</span></a></li>
+							<li><a class="tab6" href="http://questionablecontent.net/">Questionable Content<span class="right">q</span></a></li>
+							<li><a class="tab6" href="http://smbc-comics.com/">Saturday Morning Breakfast Cereal<span class="right">s</span></a></li>
+							<li><a class="tab6" href="http://theoatmeal.com/comics/">The Oatmeal<span class="right">o</span></a></li>
+						</ul>
+					</li>
+				</div>
+				<div class="clear"></div>
+				<p class="center quote"></p>
+			</ul>
+		</div>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.0.2/jquery.simpleWeather.min.js"></script>
+		<script src="mousetrap.min.js"></script>
+		<script src="script.js"></script>
+	</body>
+</html>
---
+++ script.js
@@ -0,0 +1,126 @@
+var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists." ];
+var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla" ];
+var links  = [ 'https://facebook.com/', 'https://messenger.com/', 'https://instagram.com/', 'https://tumblr.com/', 'https://twitter.com/', 'https://mail.google.com/','https://youtube.com/','https://soundcloud.com/','https://music.google.com/','https://play.spotify.com/','https://reddit.com/','https://reddit.com/r/trees/','https://reddit.com/r/see/','https://reddit.com/r/blackpeopletwitter/','https://reddit.com/r/showerthoughts/','https://reddit.com/r/unixporn/','https://reddit.com/r/startpages/','https://imgur.com/','https://gfycat.com/','https://pastebin.com/','https://stackoverflow.com/','https://developer.mozilla.com/', 'https://github.com/', 'https://askubuntu.com/', 'https://www.google.com/fonts/', 'https://unsplash.com/', 'http://en.wikipedia.org/', 'http://wolframalpha.com/', 'https://niice.co/', 'https://www.google.com/images/', 'http://life.o2dca.com/', 'http://admin.o2dca.com/', 'https://www.toggl.com/app/timer', 'http://bugs.o2dca.com/', 'https://mail.google.com/mail/u/1/#inbox', 'http://xkcd.com/', 'http://explosm.net/', 'http://potshotcomic.com/', 'http://powernapcomic.com/', 'http://extrafabulouscomics.com/', 'http://thedoghousediaries.com/', 'http://questionablecontent.net/', 'http://smbc-comics.com/', 'http://theoatmeal.com/comics/' ];
+var randNum = Math.floor((Math.random()*11)), randNum2 = Math.floor((Math.random()*22)+1);
+
+function getWeather(location) {
+	$.simpleWeather({
+		location: location,
+		success: function(weather) {
+			$('.location').html(weather.city + ', ' + weather.region);
+			$('.current').html(weather.currently);
+			$('.temp').html(weather.temp);
+			$('.wind').html(weather.wind.speed + weather.units.speed + ' ' + weather.wind.direction);
+		},
+		error: function(error) { console.log(error); }
+	});
+}
+
+Mousetrap.bind('esc', function(e) {
+	$('.subMenu').slideUp('fast');
+	$('li a').removeClass('active');
+	return false;
+});
+Mousetrap.bind('s', function(e) {
+	$('a#parent1').next().slideToggle('fast');
+	$('a#parent1').toggleClass('active');
+	Mousetrap.bind('f', function(e) { window.location.href = links[0]; });
+	Mousetrap.bind('m', function(e) { window.location.href = links[1]; });
+	Mousetrap.bind('i', function(e) { window.location.href = links[2]; });
+	Mousetrap.bind('t', function(e) { window.location.href = links[3]; });
+	Mousetrap.bind('w', function(e) { window.location.href = links[4]; });
+	Mousetrap.bind('g', function(e) { window.location.href = links[5]; });
+	return false;
+});
+Mousetrap.bind('m', function(e) {
+	$('a#parent2').next().slideToggle('fast');
+	$('a#parent2').toggleClass('active');
+	Mousetrap.bind('y', function(e) { window.location.href = links[6]; });
+	Mousetrap.bind('c', function(e) { window.location.href = links[7]; });
+	Mousetrap.bind('g', function(e) { window.location.href = links[8]; });
+	Mousetrap.bind('s', function(e) { window.location.href = links[9]; });
+	return false;
+});
+Mousetrap.bind('r', function(e) {
+	$('a#parent3').next().slideToggle('fast');
+	$('a#parent3').toggleClass('active');
+	Mousetrap.bind('f', function(e) { window.location.href = links[10]; });
+	Mousetrap.bind('t', function(e) { window.location.href = links[11]; });
+	Mousetrap.bind('s', function(e) { window.location.href = links[12]; });
+	Mousetrap.bind('b', function(e) { window.location.href = links[13]; });
+	Mousetrap.bind('w', function(e) { window.location.href = links[14]; });
+	Mousetrap.bind('u', function(e) { window.location.href = links[15]; });
+	Mousetrap.bind('x', function(e) { window.location.href = links[16]; });
+	Mousetrap.bind('i', function(e) { window.location.href = links[17]; });
+	Mousetrap.bind('g', function(e) { window.location.href = links[18]; });
+	Mousetrap.bind('p', function(e) { window.location.href = links[19]; });
+	return false;
+});
+Mousetrap.bind('?', function(e) {
+	$('a#parent4').next().slideToggle('fast');
+	$('a#parent4').toggleClass('active');
+	Mousetrap.bind('s', function(e) { window.location.href = links[20]; });
+	Mousetrap.bind('m', function(e) { window.location.href = links[21]; });
+	Mousetrap.bind('g', function(e) { window.location.href = links[22]; });
+	Mousetrap.bind('u', function(e) { window.location.href = links[23]; });
+	Mousetrap.bind('f', function(e) { window.location.href = links[24]; });
+	Mousetrap.bind('p', function(e) { window.location.href = links[25]; });
+	Mousetrap.bind('w', function(e) { window.location.href = links[26]; });
+	Mousetrap.bind('a', function(e) { window.location.href = links[27]; });
+	Mousetrap.bind('n', function(e) { window.location.href = links[28]; });
+	Mousetrap.bind('i', function(e) { window.location.href = links[29]; });
+	return false;
+});
+Mousetrap.bind('o', function(e) {
+	$('a#parent5').next().slideToggle('fast');
+	$('a#parent5').toggleClass('active');
+	Mousetrap.bind('l', function(e) { window.location.href = links[30]; });
+	Mousetrap.bind('a', function(e) { window.location.href = links[31]; });
+	Mousetrap.bind('t', function(e) { window.location.href = links[32]; });
+	Mousetrap.bind('m', function(e) { window.location.href = links[33]; });
+	Mousetrap.bind('g', function(e) { window.location.href = links[34]; });
+	return false;
+});
+Mousetrap.bind('c', function(e) {
+	$('a#parent6').next().slideToggle('fast');
+	$('a#parent6').toggleClass('active');
+	Mousetrap.bind('x', function(e) { window.location.href = links[35]; });
+	Mousetrap.bind('&', function(e) { window.location.href = links[36]; });
+	Mousetrap.bind('p', function(e) { window.location.href = links[37]; });
+	Mousetrap.bind('n', function(e) { window.location.href = links[38]; });
+	Mousetrap.bind('e', function(e) { window.location.href = links[39]; });
+	Mousetrap.bind('d', function(e) { window.location.href = links[40]; });
+	Mousetrap.bind('q', function(e) { window.location.href = links[41]; });
+	Mousetrap.bind('s', function(e) { window.location.href = links[42]; });
+	Mousetrap.bind('o', function(e) { window.location.href = links[43]; });
+	return false;
+});
+Mousetrap.bind('space', function(e){
+	$('.subMenu').slideUp('fast');
+	$('li a').removeClass('active');
+	randNum = Math.floor((Math.random()*11));
+	$('.quote').html("<p>&ldquo;" + quotes[randNum] + "&rdquo;</p>" + "<cite><p><small>" + quoted[randNum] + "</small></p></cite>");
+	randNum2 = Math.floor((Math.random()*22)+1);
+	$('body').css('background-image', 'url(/img/trippy/bg' + randNum2 + '.png)');
+	if("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+	    	getWeather(position.coords.latitude+','+position.coords.longitude);
+	  	});
+	} else { getWeather("Pittsburgh, PA"); }
+	return false;
+});
+
+$(function() {
+	var a=document.getElementById("snow"),d=a.getContext("2d"),e=[],f=Math;a.style.pointerEvents="none";a.style.position="fixed";a.style.width="100vw";a.style.height="100vh";a.height=a.offsetHeight;a.width=a.offsetWidth;window.onresize=function(){a.height=a.offsetHeight;a.width=a.offsetWidth}; setInterval(function(){d.clearRect(0,0,a.width,a.height);d.beginPath();if(.3<f.random()){var b=f.random(),g=.05+.95*b,c={};c.x=1.5*a.width*f.random()-.5*a.width;c.y=-9;c.c=2*g*(f.random()/2+.5);c.d=5*g;c.a=5*b;c.b=function(){var t=this;t.x+=t.c;t.y+=t.d;d.beginPath();d.arc(t.x,t.y,t.a,0,2*f.PI,!1);d.fillStyle="#FFF";d.fill()};e.push(c)}for(b=0;b<e.length;b++)e[b].y>a.height?e.splice(b,1):e[b].b()},16);
+	$('.quote').html("<p>&ldquo;" + quotes[randNum] + "&rdquo;</p>" + "<cite><p><small>" + quoted[randNum] + "</small></p></cite>");
+	if("geolocation" in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+	    	getWeather(position.coords.latitude+','+position.coords.longitude);
+	  	});
+	} else { getWeather("Pittsburgh, PA"); }
+	$("ul.subMenu").hide();
+	$("li:has(ul)").click(function(){
+		$("ul",this).slideToggle('fast');
+		$("a.parent", this).toggleClass('active');
+	});
+});
