c4ce2b0f3158d746805dad580c2145e145305872
Paul Schaefer <yeppaul2@gmail.com>
2016-05-05 11:07:18 -0400

    added link, reworked JS

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var TOTAL_PRESETS = 15; // Update this when adding new quotes, greetings, or backgrounds
-var quotes = [ "If you are depressed you are living in thev past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
+var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
 var greets = [ 'Hello', 'Howdy', 'Yo', 'Sup', 'Wazzup', 'Salutations', 'Hey', 'Hi', 'Greetings', 'Aloha', 'Namaste', 'Hiya', 'Yello', 'Holla', 'Peace' ];
 
@@ -7,8 +7,13 @@ var greets = [ 'Hello', 'Howdy', 'Yo', 'Sup', 'Wazzup', 'Salutations', 'Hey', 'H
 function getWeather(location) {
 	$.simpleWeather({
 		location: location,
-		success:  function(weather) { $('.weather').html('In ' + weather.city + ', ' + weather.region + ', the weather is ' + weather.currently + ',<br>the temperature is ' + weather.temp + '&deg;, and the wind is ' + weather.wind.speed + weather.units.speed + ' ' + weather.wind.direction); },
-		error:    function(error)   { $('.weather').html(error); }
+		success: function(weather) {
+			$('.weather').html('In ' + weather.city + ', ' + weather.region + ', the weather is ' + weather.currently + ',<br>the temperature is ' + weather.temp + '&deg;, and the wind is ' + weather.wind.speed + weather.units.speed + ' ' + weather.wind.direction);
+		},
+		error: function(error)   {
+			$('.weather').html('Sorry, there has been a problem retrieving the weather information.');
+			console.log(error);
+		}
 	});
 }
 
@@ -47,33 +52,38 @@ function bindMousetraps() {
 			});
 		});
 	});
+
+	// Esc to close all tabs, leave secret mode
+	Mousetrap.bind('esc', function(e) {
+		var randNum = Math.floor((Math.random() * TOTAL_PRESETS));
+		$('body').removeClass('rnbw').attr('class', function(i, c) {
+			return c.replace(/(^|\s)bg\S+/g, '');
+		}).addClass('bg' + (randNum + 1));
+		$('.subMenu').slideUp(150);
+		$('li a').removeClass('active');
+		Mousetrap.reset();
+		bindMousetraps();
+		console.log('you escaped!');
+		return false;
+	});
+
+	// Refreshes everything, and closes all cells
+	Mousetrap.bind('space', function(e){
+		$('.subMenu').slideUp(150);
+		$('li a').removeClass('active');
+		refreshStuffs();
+		console.log('manually refreshed!');
+		return false;
+	});
+
+	// SECRET PARTY MODE!!1! :D
+	Mousetrap.bind('up up down down left right left right b a enter', function() {
+		$('body').attr('class', function(i, c){
+	    	return c.replace(/(^|\s)bg\S+/g, '');
+		}).addClass('rnbw');
+		console.log('PARTYYYYYYY :D');
+	});
 }
-
-// Esc to close all tabs, leave secret mode
-Mousetrap.bind('esc', function(e) {
-	var randNum = Math.floor((Math.random() * TOTAL_PRESETS));
-	$('body').removeClass('rnbw').attr('class', function(i, c) {
-		return c.replace(/(^|\s)bg\S+/g, '');
-	}).addClass('bg' + (randNum + 1));
-	$('.subMenu').slideUp(150);
-	$('li a').removeClass('active');
-	return false;
-});
-
-// Refreshes everything, and closes all cells
-Mousetrap.bind('space', function(e){
-	$('.subMenu').slideUp(150);
-	$('li a').removeClass('active');
-	refreshStuffs();
-	return false;
-});
-
-// SECRET PARTY MODE!!1! :D
-Mousetrap.bind('up up down down left right left right b a enter', function() {
-	$('body').attr('class', function(i, c){
-    	return c.replace(/(^|\s)bg\S+/g, '');
-	}).addClass('rnbw');
-});
 
 // Does everything on page load, sets it to auto-refresh every 30s
 $(function() {
@@ -84,5 +94,8 @@ $(function() {
 	});
 	refreshStuffs();
 	bindMousetraps();
-	setInterval(function(){ refreshStuffs(); }, 30000);
+	setInterval(function(){
+		console.log('30s has passed, refreshing...');
+		refreshStuffs();
+	}, 30000);
 });
