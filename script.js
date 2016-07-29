e74bb3442ba7c5435563c15c6bb3f8f414e81c55
Paul Schaefer <yeppaul2@gmail.com>
2016-07-29 14:39:37 -0400

    added background click to close, restructured JS

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
 var greets = [ 'Hello', 'Howdy', 'Yo', 'Sup', 'Wazzup', 'Salutations', 'Hey', 'Hi', 'Greetings', 'Aloha', 'Namaste', 'Hiya', 'Yello', 'Holla', 'Peace' ];
-var TOTAL_PRESETS = greets.length; // NOTE: keep the number of greetings and quotes the same
-// Finds current time and date
+
+
+
+// Finds current time and date, formats it properly
 function startTime() {
 	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	var now = new Date();
@@ -33,6 +35,9 @@ function getNonMilitary(i) {
 		i = 12;
 	return i;
 }
+
+
+
 // Gets weather for requested location, appends to page
 function getWeather(location) {
 	$.simpleWeather({
@@ -42,30 +47,29 @@ function getWeather(location) {
 			$('.weatherlink').html('<a href="' + weather.link + '">More details (w)</a>');
 		},
 		error: function(error)   {
-			$('.weatherlink').html('Sorry, there has been a problem retrieving the weather information.');
+			$('.weather').html('Sorry, there has been a problem retrieving the weather information.');
 		}
 	});
 }
+
+
+
 // Master loading function; appends random greeting, quote, and weather
 function loadStuff() {
-	var randNum = Math.floor((Math.random() * TOTAL_PRESETS));
+	var randNum = Math.floor((Math.random() * greets.length));
 	$('.greeting').html(greets[randNum]);
 	$('.quote').html('&ldquo;' + quotes[randNum] + '&rdquo; &mdash; ' + '<cite><small>' + quoted[randNum] + '</small></cite>');
 	// Geolocates the user, otherwise defaulting to Pittsburgh (2473224)
-	console.log('right before geolocation');
 	if('geolocation' in navigator) {
 		navigator.geolocation.getCurrentPosition(function(position) {
 	    	getWeather(position.coords.latitude + ',' + position.coords.longitude);
-			console.log('geolocation successful');
-	  	}, geolocationFailure(), {timeout: 5000});
-	} else { geolocationFailure(); }
-
-	function geolocationFailure() {
-		getWeather(2473224);
-		console.log('geolocation not successful');
-	}
+	  	}, getWeather(2473224), {timeout: 5000});
+	} else { getWeather(2473224); }
 }
-// Initializes main keyboard nav
+
+
+
+// Initializes keyboard nav
 function bindMousetraps() {
 	$.each($('.parent'), function(i, val) {
 		Mousetrap.bind($(val).children('span').text(), function(e) {
@@ -76,31 +80,15 @@ function bindMousetraps() {
 				});
 			});
 			Mousetrap.bind($(val).children('span').text(), function(e) {
-				$('.subMenu').slideUp(150);
-				$('li a').removeClass('active');
-				Mousetrap.reset();
-				bindMousetraps();
-				return false;
+				resetMousetraps();
 			});
 		});
 	});
-	// Esc to close all tabs
-	Mousetrap.bind('esc', function(e) {
-		var randNum = Math.floor((Math.random() * TOTAL_PRESETS));
-		$('.subMenu').slideUp(150);
-		$('li a').removeClass('active');
-		Mousetrap.reset();
-		bindMousetraps();
-		return false;
+	// Resets on ESC or spacebar
+	Mousetrap.bind(['esc', 'space'], function(e) {
+		resetMousetraps();
 	});
-	// Closes all cells and reloads stuff
-	Mousetrap.bind('space', function(e) {
-		$('.subMenu').slideUp(150);
-		$('li a').removeClass('active');
-		loadStuff();
-		return false;
-	});
-	// Binds weather detail and GitHub links
+	// Binds Weather and GitHub links
 	Mousetrap.bind('w', function(e) {
 		window.location.href = $('.weatherlink').children().attr('href');
 	});
@@ -108,13 +96,30 @@ function bindMousetraps() {
 		window.location.href = $('.github').children().attr('href');
 	});
 }
-// Does everything on page load
+
+
+
+// Closes cells, rebinds keyboard shortcuts
+function resetMousetraps() {
+	$('.subMenu').slideUp(150);
+	$('li a').removeClass('active');
+	Mousetrap.reset();
+	bindMousetraps();
+}
+
+
+
+// Initializes everything on page load
 $(function() {
 	startTime();
 	loadStuff();
 	bindMousetraps();
+	// Binds click events for opening tabs and background click to close
 	$('li a.parent').click(function() {
 		$(this).parent('li').find('ul').slideToggle(150);
 		$(this).toggleClass('active');
+	});
+	$('#background').click(function() {
+		resetMousetraps();
 	});
 });
