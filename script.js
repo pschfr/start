30426c95cbceea67df028857d9c64153d10dc6ac
Paul Schaefer <yeppaul2@gmail.com>
2016-08-12 11:58:18 -0400

    added day of the week

--- script.js
+++ script.js
@@ -6,13 +6,15 @@ var greets = [ 'Hello', 'Howdy', 'Yo', 'Sup', 'Wazzup', 'Salutations', 'Hey', 'H
 
 // Finds current time and date, formats it properly
 function startTime() {
-	var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+	var dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 	var now = new Date();
 	var hour = now.getHours();
 	var mins = now.getMinutes();
 	var secs = now.getSeconds();
 	var ampm = hour >= 12 ? 'PM' : 'AM';
-	var day = now.getDate();
+	var date = now.getDate();
+	var day  = dayNames[now.getDay()];
 	var month = monthNames[now.getMonth()];
 	var year = now.getFullYear();
 	hour = hour % 12;
@@ -20,7 +22,7 @@ function startTime() {
 	mins = mins < 10 ? '0' + mins : mins;
 	secs = secs < 10 ? '0' + secs : secs;
 	var timeString = hour + ':' + mins + ':' + secs + ' ' + ampm;
-	var dateString = month + ' ' + day + ', ' + year;
+	var dateString = day + ' ' + month + ' ' + date + ', ' + year;
 	document.getElementById('time').innerHTML = timeString;
 	document.getElementById('date').innerHTML = dateString;
 	var t = setTimeout(startTime, 500);
