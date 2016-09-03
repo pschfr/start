d6c15d9b3552c03b689202709b8ba9a986041220
Paul Schaefer <yeppaul2@gmail.com>
2016-09-03 13:34:39 -0400

    tidied code a lot, changed it to system sans-serif font

--- script.js
+++ script.js
@@ -1,35 +1,28 @@
+// Important: keep the number of quotes and greetings the same
 var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
-var greets = [ 'Hello', 'Howdy', 'Yo', 'Sup', 'Wazzup', 'Salutations', 'Hey', 'Hi', 'Greetings', 'Aloha', 'Namaste', 'Hiya', 'Yello', 'Holla', 'Peace' ];
-
-
-
+var greets = [ "Hello", "Howdy", "Yo", "Sup", "Wazzup", "Salutations", "Hey", "Hi", "Greetings", "Aloha", "Namaste", "Hiya", "Yello", "Holla", "Peace" ];
 // Finds current time and date, formats it properly
 function startTime() {
 	var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
-	var dayNames = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-	var now = new Date();
-	var hour = now.getHours();
-	var mins = now.getMinutes();
-	var secs = now.getSeconds();
-	var ampm = hour >= 12 ? 'PM' : 'AM';
-	var date = now.getDate();
-	var day  = dayNames[now.getDay()];
+	var dayNames   = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+	var now   = new Date();
+	var hour  = now.getHours();
+	var mins  = now.getMinutes();
+	var secs  = now.getSeconds();
+	var ampm  = hour >= 12 ? 'PM' : 'AM';
+	var date  = now.getDate();
+	var day   = dayNames[now.getDay()];
 	var month = monthNames[now.getMonth()];
-	var year = now.getFullYear();
+	var year  = now.getFullYear();
 	hour = hour % 12;
   	hour = hour ? hour : 12;
 	mins = mins < 10 ? '0' + mins : mins;
 	secs = secs < 10 ? '0' + secs : secs;
-	var timeString = hour + ':' + mins + ':' + secs + ' ' + ampm;
-	var dateString = day + ' ' + month + ' ' + date + ', ' + year;
-	document.getElementById('time').innerHTML = timeString;
-	document.getElementById('date').innerHTML = dateString;
+	document.getElementById('time').innerHTML = hour + ':' + mins + ':' + secs + ' ' + ampm;
+	document.getElementById('date').innerHTML = day + ' ' + month + ' ' + date + ', ' + year;
 	var t = setTimeout(startTime, 500);
 }
-
-
-
 // Gets weather for requested location, appends to page
 function getWeather(location) {
 	$.simpleWeather({
@@ -38,14 +31,11 @@ function getWeather(location) {
 			$('.weather').html('In ' + weather.city + ', ' + weather.region + ', the weather is ' + weather.currently + ', the temperature is ' + weather.temp + '&deg;, and the wind is ' + weather.wind.speed + weather.units.speed + ' <span class="no-transform">' + weather.wind.direction + '</span>');
 			$('.weatherlink').html('<a href="' + weather.link + '">More details (w)</a>');
 		},
-		error: function(error)   {
-			$('.weather').html('Sorry, there has been a problem retrieving the weather information.');
+		error: function(error) {
+			$('.weather').html('Sorry, ' + error);
 		}
 	});
 }
-
-
-
 // Master loading function; appends random greeting, quote, and weather
 function loadStuff() {
 	var randNum = Math.floor((Math.random() * greets.length));
@@ -58,9 +48,6 @@ function loadStuff() {
 	  	}, getWeather(2473224), {timeout: 5000});
 	} else { getWeather(2473224); }
 }
-
-
-
 // Initializes keyboard nav
 function bindMousetraps() {
 	$.each($('.parent'), function(i, val) {
@@ -88,9 +75,6 @@ function bindMousetraps() {
 		window.location.href = $('.github').children().attr('href');
 	});
 }
-
-
-
 // Closes cells, rebinds keyboard shortcuts
 function resetMousetraps() {
 	$('.subMenu').slideUp(150);
@@ -98,9 +82,6 @@ function resetMousetraps() {
 	Mousetrap.reset();
 	bindMousetraps();
 }
-
-
-
 // Initializes everything on page load
 $(function() {
 	startTime();
