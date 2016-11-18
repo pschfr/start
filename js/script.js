9a46d5e75fa4df0ad854a2ae25a809764d1dce84
Paul Schaefer <yeppaul2@gmail.com>
2016-11-18 11:30:03 -0500

    replaced jQuery click events with vanilla JS

--- js/script.js
+++ js/script.js
@@ -19,27 +19,24 @@ function startTime() {
 	document.getElementById('time').innerHTML = hour + ':' + mins + ':' + secs + ' ' + ampm + '<br/>' + weekday + ', ' + month + '. ' + day;
 	var t = setTimeout(startTime, 500);
 }
-
 // Random quote function. Important: Make sure each quote has a corresponding "quoted".
 function randomQuote() {
-	$('#quote').unbind('click');
+	document.getElementById('quote').removeEventListener('click', randomQuote, false);
 	var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 	var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
 	var randNumQuotes = Math.floor((Math.random() * quotes.length));
 	document.getElementById('quote').innerHTML = '&ldquo;' + quotes[randNumQuotes] + '&rdquo;<br />' + '<small>' + quoted[randNumQuotes] + '</small>';
-	$('#quote').click(function() { randomQuote(); });
+	document.getElementById('quote').addEventListener('click', randomQuote, false);
 }
-
 // Gets weather for requested location, appends to page
 function getWeather(location) {
 	$.simpleWeather({
 		location: location,
 		success: function(weather) {
-			document.getElementById('weather').innerHTML = '<a href="' + weather.link + '" title="' + weather.wind.speed + weather.units.speed + ' ' + weather.wind.direction + '" id="weatherlink">' + weather.temp + '&deg;, ' + weather.currently + '<br/>' + weather.city + ',' + weather.region + '</a>';
+			document.getElementById('weather').innerHTML = '<a href="' + weather.link + '" title="Wind: ' + weather.wind.speed + weather.units.speed + ' ' + weather.wind.direction + '" id="weatherlink">' + weather.temp + '&deg;, ' + weather.currently + '<br/>' + weather.city + ',' + weather.region + '</a>';
 		}
 	});
 }
-
 // Geolocates the user, otherwise defaulting to Pittsburgh (2473224)
 function geolocWeather() {
 	if('geolocation' in navigator) {
@@ -48,7 +45,6 @@ function geolocWeather() {
 	  	}, getWeather(2473224), {timeout: 5000});
 	} else { getWeather(2473224); }
 }
-
 // Initializes keyboard nav
 function bindMousetraps() {
 	// Loops through parent cells, opening those on request
@@ -81,7 +77,6 @@ function bindMousetraps() {
 		window.location.href = 'https://github.com/pschfr/start';
 	});
 }
-
 // Closes cells, rebinds keyboard shortcuts
 function resetMousetraps() {
 	$('.subMenu').slideUp(150);
@@ -89,9 +84,8 @@ function resetMousetraps() {
 	Mousetrap.reset();
 	bindMousetraps();
 }
-
 // Connects to Last.FM, retrives most recent song
-function lastFM_request() {
+function lastfmRequest() {
 	var username  = 'paul_r_schaefer';
 	var API_key   = '0f680404e39c821cac34008cc4d803db';
 	var lastFMurl = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + username + '&api_key=' + API_key + '&limit=1&format=json';
@@ -101,34 +95,33 @@ function lastFM_request() {
 	xmlhttp.onreadystatechange = function() {
 	    if (xmlhttp.readyState == 4) {
 	        if(xmlhttp.status == 200) {
+				var total = JSON.parse(xmlhttp.responseText).recenttracks['\@attr'].total;
 	            var track = JSON.parse(xmlhttp.responseText).recenttracks.track[0];
 
 				if (track['\@attr'] && track['\@attr'].nowplaying !== '')
-					element.innerHTML = 'currently listening to: ';
+					element.innerHTML = '<span title="' + total + ' total streamed">currently listening to:</span> ';
 				else
-					element.innerHTML = 'last listened to: ';
+					element.innerHTML = '<span title="' + total + ' total streamed">last listened to:</span> ';
 
 				element.innerHTML += '<a href="' + track.url + '" title="on album: ' + track.album['\#text'] + '">' + track.artist['\#text'] + ' &mdash; ' + track.name + '</a> ';
 	         }
 	    }
 	};
 	xmlhttp.send(null);
-}
 
+	var t = setTimeout(startTime, 15000);
+}
 // Initializes everything on page load
 $(function() {
 	startTime();
 	randomQuote();
 	bindMousetraps();
 	geolocWeather();
-	lastFM_request();
-
+	lastfmRequest();
 	// Binds click events for opening tabs and background click to close
 	$('li a.parent').click(function() {
 		$(this).parent('li').find('ul').slideToggle(150);
 		$(this).toggleClass('active');
 	});
-	$('#background').click(function() {
-		resetMousetraps();
-	});
+	document.getElementById('background').addEventListener('click', resetMousetraps, false);
 });
