fdd6bd63c9155efa0e2a6d558ab87d6e3b70ddd4
Paul Schaefer <yeppaul2@gmail.com>
2016-11-18 10:49:51 -0500

    changed background image to refresh daily, added click to advance quote

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # start [https://pschfr.github.io/start/](https://pschfr.github.io/start/)
 ![https://pschfr.github.io/start/](screenshot.png)
-##Personal new tab page with keyboard nav, bookmarks, weather, quote, most recently listened to song, and image background.
+##Personal new tab page with keyboard navigation, bookmarks, weather, quote, most recently listened to song, and image background.
 
 ##Features:
-- Background image randomizes on page load, from [Unsplash Source](https://source.unsplash.com/) which pulls from [this collection](https://unsplash.com/collections/292287/)
-- Weather is from [monkeecreate/jquery.simpleWeather](https://github.com/monkeecreate/jquery.simpleWeather)
+- Background is a random nature photo that changes daily from [Unsplash Source](https://source.unsplash.com/)
+- Weather is from Yahoo Weather via [monkeecreate/jquery.simpleWeather](https://github.com/monkeecreate/jquery.simpleWeather)
 - Keyboard navigation is thanks to [ccampbell/mousetrap](https://github.com/ccampbell/mousetrap)!
 - Random inspirational quote!
 - Most recently listened to song thanks to my Last.FM script [pschfr/LastFM.js](https://github.com/pschfr/LastFM.js)!
 
 Each category is openable by keyboard and click, press escape or click background to close all.
 
-##You need extensions for most browsers to use this as your new tab page:
+###You need extensions for most browsers to use this as your new tab page:
 - [New Tab Redirect for Chrome](https://chrome.google.com/webstore/detail/new-tab-redirect/icpgjfneehieebagbmdbhnlpiopdcmna)
 	- You can also go to chrome://extensions, tick Developer Mode, and Load as unpacked extension for faster loading.
 - [New Tab Override for Firefox](https://addons.mozilla.org/en-US/firefox/addon/new-tab-override/)
@@ -20,8 +20,8 @@ Each category is openable by keyboard and click, press escape or click backgroun
 
 Browse similar style pages on [/r/startpages](https://reddit.com/r/startpages).
 
-BTW, it's super fast. Fully loaded at ~300ms (~150ms as an extension!!) :D
+It's super fast. Fully loaded at ~300ms (~150ms as an extension!!) :D
 
 Feel free to fork, and add your own bookmarks and quotes!
 
-[@pschfr](http://twitter.com/pschfr)
+If you have any questions, [tweet me](http://twitter.com/pschfr) or file an issue or pull request.
--- js/script.js
+++ js/script.js
@@ -20,14 +20,14 @@ function startTime() {
 	var t = setTimeout(startTime, 500);
 }
 
-// Random quote
+// Random quote function. Important: Make sure each quote has a corresponding "quoted".
 function randomQuote() {
-	// Important: Make sure each quote has a corresponding "quoted"
+	$('#quote').unbind('click');
 	var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 	var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
 	var randNumQuotes = Math.floor((Math.random() * quotes.length));
-
-	document.getElementById('quote').innerHTML = '&ldquo;' + quotes[randNumQuotes] + '&rdquo; &mdash; ' + '<cite><small>' + quoted[randNumQuotes] + '</small></cite>';
+	document.getElementById('quote').innerHTML = '&ldquo;' + quotes[randNumQuotes] + '&rdquo;<br />' + '<small>' + quoted[randNumQuotes] + '</small>';
+	$('#quote').click(function() { randomQuote(); });
 }
 
 // Gets weather for requested location, appends to page
