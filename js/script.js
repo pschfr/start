2b2e7bde0e72b5076f6e6600721a85d476e8f21d
Paul Schaefer <yeppaul2@gmail.com>
2016-11-19 19:32:37 -0500

    fixed weather bugs, tweaked JS

--- js/script.js
+++ js/script.js
@@ -26,34 +26,8 @@ function randomQuote() {
 	var quotes = [ "If you are depressed you are living in the past. If you are anxious you are living in the future. If you are at peace you are living in the present.", "Madness, as you know, is a lot like gravity, all it takes is a little push.", "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.", "Life has many ways of testing a person's will, either by having nothing happen at all or by having everything happen all at once.", "There is no excellent beauty that hath not some strangeness in its proportions.", "Children are fantastic little creatures, because next to drunk people, they are the only truly honest people on earth.", "I begin with an idea, and then it becomes something else.", "Be who you are and say what you feel because those who mind don't matter and those who matter don't mind.", "You can make more friends in two months by becoming interested in other people than you can in two years by trying to get people interested in you.", "An essential aspect of creativity is not being afraid to fail.", "Antisocial behavior is a trait of intelligence in a world of conformists.", "What you do today can improve all your tomorrows.", "A creative man is motivated by the desire to achieve, not by the desire to beat others.", "Don't watch the clock; do what it does. Keep going.", "If you can dream it, you can do it.", "You can't build a reputation on what you're going to do." ];
 	var quoted = [ "Lao Tzu", "Joker", "Friedrich Nietzsche", "Paulo Coelho", "Sir Francis Bacon", "Mads Nipper", "Pablo Picasso", "Dr. Seuss", "Dale Carnegie", "Edwin Land", "Nikola Tesla", "Ralph Marston", "Ayn Rand", "Sam Levenson", "Walt Disney", "Henry Ford" ];
 	var randNumQuotes = Math.floor((Math.random() * quotes.length));
-	document.getElementById('quote').innerHTML = '&ldquo;' + quotes[randNumQuotes] + '&rdquo;<br />' + '<small>' + quoted[randNumQuotes] + '</small>';
+	document.getElementById('quote').innerHTML = '&ldquo;' + quotes[randNumQuotes] + '&rdquo; &mdash; ' + '<small>' + quoted[randNumQuotes] + '</small>';
 	document.getElementById('quote').addEventListener('click', randomQuote, false);
-}
-// Gets weather for requested location, appends to page
-function getWeather(location) {
-	var API_key   = '3dc48ab835ed1b4369c089d0e742ff03';
-	var darkSkyURL = 'https://api.darksky.net/forecast/' + API_key + '/' + location + '?exclude=flags';
-	var element   = document.getElementById('weather');
-	var xmlhttp   = new XMLHttpRequest();
-	xmlhttp.open('GET', darkSkyURL, true);
-	xmlhttp.onreadystatechange = function() {
-	    if (xmlhttp.readyState == 4) {
-	        if(xmlhttp.status == 200) {
-				var weather = JSON.parse(xmlhttp.responseText);
-				console.log(weather);
-					element.innerHTML = '<span title="' + weather.hourly.summary + '">' + Math.trunc(weather.currently.temperature) + '&deg;, ' + weather.currently.summary + '</span>';
-	         }
-	    }
-	};
-	xmlhttp.send(null);
-}
-// Geolocates the user, otherwise defaulting to Pittsburgh
-function geolocWeather() {
-	if('geolocation' in navigator) {
-		navigator.geolocation.getCurrentPosition(function(position) {
-	    	getWeather(position.coords.latitude + ',' + position.coords.longitude);
-	  	});
-	} else { getWeather('Pittsburgh, PA'); }
 }
 // Loop through the user's first 6 bookmark folders
 function fetchBookmarks() {
@@ -114,6 +88,32 @@ function resetMousetraps() {
 	$('li a').removeClass('active');
 	Mousetrap.reset();
 	bindMousetraps();
+}
+// Gets weather for requested location, appends to page
+function getWeather(location) {
+	var API_key   = '3dc48ab835ed1b4369c089d0e742ff03';
+	var darkSkyURL = 'https://api.darksky.net/forecast/' + API_key + '/' + location + '?exclude=flags';
+	var element   = document.getElementById('weather');
+	var xmlhttp   = new XMLHttpRequest();
+	xmlhttp.open('GET', darkSkyURL, true);
+	xmlhttp.onreadystatechange = function() {
+	    if (xmlhttp.readyState == 4) {
+	        if(xmlhttp.status == 200) {
+				var weather = JSON.parse(xmlhttp.responseText);
+				// console.log(weather);
+				element.innerHTML = '<a href="https://darksky.net/forecast/' + location + '" title="' + weather.hourly.summary + '">' + weather.currently.summary + ', ' + Math.trunc(weather.currently.temperature) + '&deg;</a>';
+	        }
+	    }
+	};
+	xmlhttp.send(null);
+}
+// Geolocates the user, otherwise defaulting to Pittsburgh
+function geolocWeather() {
+	if('geolocation' in navigator) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+	    	getWeather(position.coords.latitude + ',' + position.coords.longitude);
+	  	});
+	} else { getWeather('40.4406, -79.9959'); }
 }
 // Connects to Last.FM, retrives most recent song
 function lastfmRequest() {
