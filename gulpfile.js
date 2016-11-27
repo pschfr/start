415c778efb1deb1a5211741254a140409833a66d
Paul Schaefer <yeppaul2@gmail.com>
2016-11-27 14:26:03 -0500

    simplified gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,8 @@ var gulp      = require('gulp');
 var uglify    = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
 var htmlmin   = require('gulp-htmlmin');
-gulp.task('js', function() {
-	return gulp.src('src/script.js').pipe(uglify()).pipe(gulp.dest('dist/'));
-});
-gulp.task('css', function () {
-	return gulp.src('src/style.css').pipe(uglifycss()).pipe(gulp.dest('dist/'));
-});
-gulp.task('html', function() {
-	return gulp.src('src/index.html').pipe(htmlmin()).pipe(gulp.dest('dist/'));
+gulp.task('minify', function() {
+	gulp.src('src/script.js').pipe(uglify()).pipe(gulp.dest('dist/'));
+	gulp.src('src/style.css').pipe(uglifycss()).pipe(gulp.dest('dist/'));
+	gulp.src('src/index.html').pipe(htmlmin({collapseWhitespace: true, removeComments: true})).pipe(gulp.dest('dist/'));
 });
