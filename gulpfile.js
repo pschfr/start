fa13d5ebcc617a138332a5d7791b3f772f0217b7
Paul Schaefer <yeppaul2@gmail.com>
2016-11-26 22:09:00 -0500

    minified HTML with gulp too

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp  = require('gulp');
-var uglify = require('gulp-uglify');
+var gulp      = require('gulp');
+var uglify    = require('gulp-uglify');
 var uglifycss = require('gulp-uglifycss');
+var htmlmin   = require('gulp-htmlmin');
 
 gulp.task('js', function() {
 	return gulp.src('script.js')
@@ -12,4 +13,10 @@ gulp.task('css', function () {
 	return gulp.src('style.css')
 		.pipe(uglifycss())
 		.pipe(gulp.dest('min/'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('index.html')
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(gulp.dest(''));
 });
