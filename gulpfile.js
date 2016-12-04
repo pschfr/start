3a338e90567401321b08ee0a2a523ca85d95cec9
Paul Schaefer <yeppaul2@gmail.com>
2016-12-04 17:12:45 -0500

    added gulp sass function

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp      = require('gulp'),
+	sass      = require('gulp-sass')
 	concat    = require('gulp-concat'),
 	uglify    = require('gulp-uglify'),
 	uglifycss = require('gulp-uglifycss'),
 	htmlmin   = require('gulp-htmlmin');
-gulp.task('min', function() {
+gulp.task('default', function() {
 	gulp.src(['src/jquery-3.1.1.js', 'src/mousetrap.js', 'src/unsplash-source.js', 'src/script.js']).pipe(concat('scripts.js')).pipe(gulp.dest('src/'));
 	gulp.src('src/scripts.js').pipe(uglify()).pipe(gulp.dest('dist/'));
+	gulp.src('src/style.sass').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('src/'));
 	gulp.src('src/style.css').pipe(uglifycss()).pipe(gulp.dest('dist/'));
 	gulp.src('src/index.html').pipe(htmlmin({collapseWhitespace: true, removeComments: true})).pipe(gulp.dest('dist/'));
 });
