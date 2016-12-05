468c11d224edf651c36a8ca64bd14d35a6062e1c
Paul Schaefer <yeppaul2@gmail.com>
2016-12-05 00:35:30 -0500

    replaced jquery with jquery slim

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var gulp      = require('gulp'),
 	htmlmin   = require('gulp-htmlmin');
 gulp.task('default', function() {
 	// Concatenates third-party and my JS together
-	gulp.src(['src/vendor/jquery-3.1.1.js', 'src/vendor/mousetrap.js', 'src/vendor/unsplash-source.js', 'src/script.js']).pipe(concat('scripts.js')).pipe(gulp.dest('src/'));
+	gulp.src(['src/vendor/jquery-3.1.1.slim.js', 'src/vendor/mousetrap.js', 'src/vendor/unsplash-source.js', 'src/script.js']).pipe(concat('scripts.js')).pipe(gulp.dest('src/'));
 	// Minifies JS, moves to dist
 	gulp.src('src/scripts.js').pipe(uglify()).pipe(gulp.dest('dist/'));
 	// Compiles CSS from Sass partials
